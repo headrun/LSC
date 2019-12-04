993a372cc7ee7998310137d4b4bf5e3724ae6eb5
root <reachbalabhargavi@gmail.com>
2019-12-04 16:13:29 +0530

    popup CDN

--- resources/views/layout/sidebar1.blade.php
+++ resources/views/layout/sidebar1.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <title>LSC</title>
-  <!-- <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
+  <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!------ Include the above in your HEAD tag ---------->
