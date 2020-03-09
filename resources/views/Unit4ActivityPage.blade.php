78957de6a012c0672b4637255c0d5534031acaa0
BalaBhargavi <bhargavi@headrun.com>
2020-03-09 17:36:02 +0530

    activity name change U4

--- resources/views/Unit4ActivityPage.blade.php
+++ resources/views/Unit4ActivityPage.blade.php
@@ -62,7 +62,7 @@
                 <img src="https://img.icons8.com/color/2x/activity-history.png" alt="Activity1" width="110" height="110">
               </div>
               <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
-                <h3>1. Activity 1</h3>
+                <h3>Activity 1</h3>
                 <br>
                 <!-- <p>Provides a unique opportunity to leverage high profile, quality authorship into a market segment .</p> -->
               </div>
@@ -76,7 +76,7 @@
                 <img src="https://img.icons8.com/color/2x/activity-history.png" alt="Activity2" width="110" height="110">
               </div>
               <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
-                <h3>2. Activity 2</h3>
+                <h3>Activity 2</h3>
                 <br>
                 <!-- <p>Provides a unique opportunity to leverage high profile, quality authorship into a market segment .</p> -->
               </div>
@@ -93,7 +93,7 @@
                 <img src="https://img.icons8.com/color/2x/activity-history.png" alt="Activity" width="110" height="110">  
               </div>
               <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
-                <h3>3. Activity 3</h3>
+                <h3>Activity 3</h3>
                 <br>
                 <!-- <p> Economics is a social science concerned with the production, distribution, and consumption of goods and services.</p> -->
               </div>
@@ -107,7 +107,7 @@
                 <img src="https://img.icons8.com/color/2x/activity-history.png" alt="Activity" width="110" height="110">  
               </div>
               <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
-                <h3>4. Activity 4</h3>
+                <h3>Activity 4</h3>
                 <br>
                 <!-- <p> Economics is a social science concerned with the production, distribution, and consumption of goods and services.</p> -->
               </div>
